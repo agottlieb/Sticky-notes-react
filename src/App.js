2d025c5eb8fd34471b4505fa777df33d8afe2238
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-27 12:25:14 -0400

    updated noteslist and added to app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
 import Header from "./Header.js";
+import NoteList from "./NotesList.js";
 
 var App = () => (
   <div>
     <Header />
-
-    <ul className="notes-list">
-      <li className="note">
-        <input type="text" placeholder="Title" className="note__title" />
-        <textarea placeholder="Description..." className="note__description" />
-        <span className="note__delete">X</span>
-      </li>
-      <li className="note">
-        <input type="text" placeholder="Title" className="note__title" />
-        <textarea placeholder="Description..." className="note__description" />
-        <span className="note__delete">X</span>
-      </li>
-      <li className="note">
-        <input type="text" placeholder="Title" className="note__title" />
-        <textarea placeholder="Description..." className="note__description" />
-        <span className="note__delete">X</span>
-      </li>
-    </ul>
+    <NoteList />
   </div>
 );
 
---
+++ src/NotesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Note from "./Note.js";
+
+var NotesList = () => (
+  <ul className="notes-list">
+    <Note />
+    <Note />
+    <Note />
+  </ul>
+);
+
+export default NotesList;
