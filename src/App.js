853d2c4c7bb43d35894cc087e83f9804002a63af
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-27 12:55:23 -0400

    turned app into container comp to manage state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header.js";
 import NoteList from "./NotesList.js";
 
-var App = () => (
-  <div>
-    <Header />
-    <NoteList />
-  </div>
-);
-
+/*Below is the container component to manage state 
+for entire app. Rendering is passed to presentational comps. */
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "drink",
+        description: "cool refreshing water",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "listen",
+        description: "all the podcasts",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: "search for me"
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <NoteList />
+      </div>
+    );
+  }
+}
 export default App;
