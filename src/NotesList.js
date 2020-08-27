2926e74552fd7270e5c2b03c0acf4f20ce12355c
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-27 17:04:05 -0400

    added props to noteslist module

--- src/NotesList.js
+++ src/NotesList.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Note from "./Note.js";
 
-var NotesList = () => (
-  <ul className="notes-list">
-    <Note />
-    <Note />
-    <Note />
-  </ul>
-);
+const renderNote = (note) => <Note note={note} key={note.id} />;
+
+var NotesList = (props) => {
+  var noteElements = props.notes.map(renderNote);
+  return <ul className="notes-list">{noteElements} </ul>;
+};
 
 export default NotesList;
