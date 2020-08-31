568f7e5d44a4f36400d539f1a369e5a581cbf8dc
agottlieb <allison.m.gottlieb@gmail.com>
2020-08-31 12:05:03 -0400

    more notes

--- src/App.js
+++ src/App.js
@@ -20,12 +20,9 @@ class App extends Component {
     var newNotes = [newNote, ...this.state.notes];
     this.setState({ notes: newNotes });
   };
-  //event handle to update both fields of already-made sticky notes
+  //event handle to update both fields of already-made sticky note
+  //editMeId-- if the ID was changed for the note
   onType = (editMeId, updatedKey, updatedValue) => {
-    /* event handler to update text fields in sticky notes
-      - editMeId: the id of the note that the user typed in
-      - updatedKey: which field was edited? 'title' or 'description'
-      - updatedValue: new value of edited field */
     //variable that incorporates the whole process- no change, title, descr. change
     var updateIdMatch = (note) => {
       //if nothing has been edited, return the original note
